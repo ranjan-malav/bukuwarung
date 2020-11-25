6e9e72df8e9def36d850e8e302e22c5808bc5ec0
Ranjan Malav <ranjan2192@gmail.com>
2020-11-25 19:56:45 +0530

    Initial setup

---
+++ app/src/main/java/com/ranjan/malav/bukuwarung/ui/profile/ProfileFragment.kt
@@ -0,0 +1,27 @@
+package com.ranjan.malav.bukuwarung.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.ranjan.malav.bukuwarung.R
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var profileViewModel: ProfileViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        profileViewModel =
+                ViewModelProvider(this).get(ProfileViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_profile, container, false)
+        return root
+    }
+}
